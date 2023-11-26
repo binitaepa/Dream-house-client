3d30afb7d7413b10fd6d032f7d97a468aecd4e1c
binitaepa <binitaepa@gmail.com>
2023-11-26 15:59:38 +0600

    auth provider added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDvU9Ozy48zZFPnJAya7C3NZLqaQXM45q8",
+  authDomain: "dream-house-877c7.firebaseapp.com",
+  projectId: "dream-house-877c7",
+  storageBucket: "dream-house-877c7.appspot.com",
+  messagingSenderId: "920811425675",
+  appId: "1:920811425675:web:b7d271bff6ab4c0434698e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
